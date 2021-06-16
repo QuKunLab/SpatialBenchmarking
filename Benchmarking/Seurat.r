00c9e89daaf1ac1097e094563bae72177e875fc2
wenzhang <wen2017@mail.ustc.edu.cn>
2021-06-16 15:28:37 +0800

    add codes

--- Benchmarking/Seurat.r
+++ Benchmarking/Seurat.r
@@ -6,7 +6,7 @@ RNAfile = Args[6]
 
 RNA <- read.table(RNAfile,sep = '\t',header = TRUE,row.names = 1,quote = "")
 RNA <- CreateSeuratObject(counts=RNA, project='RNA', min.cells=0, min.features=0)
-Norm = Args[9]
+Norm = Args[10]
 if (Norm == 'Norm'){
     RNA <- NormalizeData(RNA)
     RNA <- FindVariableFeatures(RNA,nfeatures = 2000)
@@ -21,24 +21,43 @@ rownames(Spatial_orig) = Genes
 colnames(Spatial_orig) <- paste0(colnames(Spatial_orig), 1:ncol(Spatial_orig))
 
 
-feature.remove = Args[9]
+test_genes = Args[9]
 features <- Args[8]
+OutFile = Args[11]
+
+features <- strsplit(features,split=',')[[1]]
+test_genes <- strsplit(test_genes,split=',')[[1]]
+
 Spatial = Spatial_orig[features,]
-print (dim(Spatial))
+print(dim(Spatial))
+
 Spatial <- CreateSeuratObject(counts=Spatial, project='Spatial', min.cells=0, min.features=0)
-if (Norm == 'Norm'){
+
+Norm =  Args[10]
+if (Norm == 'norm'){
     Spatial <- NormalizeData(Spatial)
     Spatial <- FindVariableFeatures(Spatial,nfeatures = 2000)
     Spatial <- ScaleData(Spatial)
 }
 DN = 30
-if ((legth(features)-1)<30){
-        DN = (length(features))
+if ((length(features)-1)<30){
+        DN = (length(features) -1)
 }
+
 anchors <- FindTransferAnchors(reference = RNA,query = Spatial,features = features,reduction = 'cca',reference.assay = 'RNA',query.assay = 'RNA', k.filter = NA, dims = 1:DN)
+
 refdata <- GetAssayData(object = RNA,assay = 'RNA',slot = 'data')
+
+print ('OK')
+
 imputation <- TransferData(anchorset = anchors,refdata = refdata,weight.reduction = 'pca',dims = 1:DN)
+
 options(warn = -1)
-Imp_New_genes = as.data.frame(imputation@data)[feature.remove,]
-return Imp_New_genes
+
+Imp_New_genes = as.data.frame(imputation@data)[test_genes,]
+
+write.table(Imp_New_genes,file = paste0(OutFile),sep='\t',quote=F,col.names = TRUE)
+
+#return Imp_New_genes
+
 warnings('off')
